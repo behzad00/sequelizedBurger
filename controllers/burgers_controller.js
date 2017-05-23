2f75806c331fc554bda1bc4d26df77b27db62b27
behzad00 <behzadpouyanfar@gmail.com>
2017-05-22 22:55:03 -0400

    Mon1

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,6 +2,7 @@ var express = require("express");
 
 var router = express.Router();
 var burger = require("../models/burger");
+var db = require("../models")
 
 // get route -> index
 router.get("/", function(req, res) {
@@ -10,23 +11,29 @@ router.get("/", function(req, res) {
 
 router.get("/burgers", function(req, res) {
   // express callback response by calling burger.selectAllBurger
-  burger.all(function(data) {
-    // Wrapping the array of returned burgers in a object so it can be referenced inside our handlebars
-    var hbsObject = { burgers: data };
-    res.render("index", hbsObject);
+  db.Burger.findAll().then(function(burger){
+    console.log("I am here")
+    hbsObject = {burgers: burger};
+    console.log(hbsObject);
+    res.render('index', hbsObject)
+    
   });
 });
 
 // post route -> back to index
 router.post("/burgers/create", function(req, res) {
   // takes the request object using it as input for buger.addBurger
-  burger.create(req.body.burger_name, function(result) {
-    // wrapper for orm.js that using MySQL insert callback will return a log to console,
-    // render back to index with handle
-    console.log(result);
-    res.redirect("/");
+  
+    var burger = req.body;
+   console.log(burger); 
+    db.Burger.create({ 
+       Name:burger.burger_name, 
+          devoured:false  }).then(function()
+           {res.redirect('/');   })
+    
+    
   });
-});
+
 
 // put route -> back to index
 router.put("/burgers/update", function(req, res) {
--- server.js
+++ server.js
@@ -3,13 +3,20 @@ var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
 var app = express();
+var PORT = process.env.PORT || 8080;
+var db = require("./models");
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(__dirname + "/public"));
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.static("./public")); //static directory
+
+//=============================================================================================
+
 // override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
 var exphbs = require("express-handlebars");
@@ -20,11 +27,15 @@ app.engine("handlebars", exphbs({
 app.set("view engine", "handlebars");
 
 var routes = require("./controllers/burgers_controller");
+// Routes =============================================================================
 
 app.use("/", routes);
 app.use("/update", routes);
 app.use("/create", routes);
 
 // listen on port 3000
-var port = process.env.PORT || 3000;
-app.listen(port);
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
